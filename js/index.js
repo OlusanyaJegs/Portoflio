3669c7626a58aebcf8183437a2764a8be5d1726d
OlusanyaJegs <olusanya@backdrop.photo>
2022-09-21 16:41:56 +0100

    first commit after restructure

--- js/index.js
+++ js/index.js
@@ -27,3 +27,48 @@ function linkAction() {
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// services section functionality
+// variable assigned to the services modal in the html
+const modalViews = document.querySelectorAll('.services__modal');
+// variable assigned to the services button in the html
+const modalBtns = document.querySelectorAll('.services__button');
+// variable assigned to the services modal close button in the html
+const modalCloses = document.querySelectorAll('.services__modal-close');
+
+// function to target the modal sections when clicked
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+// iterate through every modal to activate click
+modalBtns.forEach((modalBtn, i) => {
+    // add an eventlistener of click
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    })
+})
+
+// iterate through every modal to activate close
+modalCloses.forEach((modalClose) => {
+    // add an eventlistener of click
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
+
+// Portfolio swiper
+var swiper = new Swiper('.mySwiper', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el:'.swiper-pagination',
+        clickable: true,
+    },
+});
